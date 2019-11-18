bf694de1b7a417ed7c56b46b223ceab02fb3b220
Artem Dudarev <dudarev@gmail.com>
2019-11-18 02:00:11 +0200

    Explicit definition of MIDDLEWARE

--- policyinsights_website/settings.py
+++ policyinsights_website/settings.py
@@ -158,7 +158,16 @@ if os.environ.get('DATABASE_URL', ''):
 
     # Simplified static file serving.
     # https://warehouse.python.org/project/whitenoise/
-    MIDDLEWARE = ['whitenoise.middleware.WhiteNoiseMiddleware', ] + list(MIDDLEWARE)
+    MIDDLEWARE = [
+        'django.middleware.security.SecurityMiddleware',
+        'whitenoise.middleware.WhiteNoiseMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ]
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Extra places for collectstatic to find static files.
